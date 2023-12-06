62f198e814dcc311d4bfb3a8f51aaf26a42f0ad1
JohnBrian <njugunajb96@gmail.com>
2023-12-06 23:34:48 +0300

    create Geca class

--- CaterpillarControlSystem.api/Operations/Geca.cs
+++ CaterpillarControlSystem.api/Operations/Geca.cs
@@ -1,6 +1,33 @@
+using CaterpillarControlSystem.api.Initializers;
+
 namespace CaterpillarControlSystem.api.Operations;
 
 public class Geca
 {
+    private List<CaterpillarSegment> _caterpillarSegments;
+    private char _direction;
+    private char[,] _area;
+    private bool _mirror;
+    private string _growthMode;
+    private Stack<string> _history;
+    private List<string> _log;
     
+
+    public Geca(int size, int xDirection, int yDirection)
+    {
+        _caterpillarSegments = new List<CaterpillarSegment>();
+        _caterpillarSegments.Add(new CaterpillarSegment(xDirection, yDirection, InitCaterpillarSegment.Head));
+        _caterpillarSegments.Add(new CaterpillarSegment(xDirection - 1, yDirection, InitCaterpillarSegment.Body));
+        _caterpillarSegments.Add(new CaterpillarSegment(xDirection - size, yDirection, InitCaterpillarSegment.Tail));
+        _direction = InitRiderCommand.Right;
+        _area = null;
+        _mirror = false;
+        _growthMode = InitCaterpillarGrowthModes.LinearGrowth;
+        _history = new Stack<string>();
+        _log = new List<string>();
+
+
+    }
+
+   
 }
