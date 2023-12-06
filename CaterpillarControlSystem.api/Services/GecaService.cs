21585b07d9087f0fdf1823e718f47fd8ae0f5a0b
JohnBrian <njugunajb96@gmail.com>
2023-12-06 23:48:12 +0300

    create geca service

---
+++ CaterpillarControlSystem.api/Services/GecaService.cs
@@ -0,0 +1,11 @@
+using CaterpillarControlSystem.api.Interfaces;
+
+namespace CaterpillarControlSystem.api.Services;
+
+public class GecaService:IGecaInterface
+{
+    public void Move(char direction)
+    {
+        throw new NotImplementedException();
+    }
+}
