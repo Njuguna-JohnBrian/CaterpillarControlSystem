9aa6a467045dc2227dd24eca0106752659292db9
JohnBrian <njugunajb96@gmail.com>
2023-12-06 23:34:32 +0300

    create CaterpillarSegment class

--- CaterpillarControlSystem.api/Operations/CaterpillarSegment.cs
+++ CaterpillarControlSystem.api/Operations/CaterpillarSegment.cs
@@ -2,5 +2,17 @@ namespace CaterpillarControlSystem.api.Operations;
 
 public class CaterpillarSegment
 {
-    
+    public CaterpillarSegment(int xDirection, int yDirection, char segmentType)
+    {
+        this.XCoordinate = xDirection;
+        this.YCoordinate = yDirection;
+        this.SegmentType = segmentType;
+    }
+
+    public int XCoordinate { get; set; }
+
+    public int YCoordinate { get; set; }
+
+
+    public char SegmentType { get; set; }
 }
