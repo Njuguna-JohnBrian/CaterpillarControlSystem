0696ffe3f8c069829d500640952480ab301ce845
JohnBrian <njugunajb96@gmail.com>
2023-12-07 05:29:28 +0300

    create CommandService

--- CaterpillarControlSystem.control/Services/CommandService.cs
+++ CaterpillarControlSystem.control/Services/CommandService.cs
@@ -1,6 +1,15 @@
+using CaterpillarControlSystem.control.model;
+
 namespace CaterpillarControlSystem.control.Services;
 
-public class CommandService
+public abstract class CommandService
 {
-    
+    public char Direction { get; }
+    public Segment Tail { get; }
+
+    protected CommandService(char direction, Segment tail)
+    {
+        Direction = direction;
+        Tail = tail;
+    }
 }
