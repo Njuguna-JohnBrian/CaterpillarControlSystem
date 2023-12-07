5b2c8e094e52baf1b02882637d725e858ee4b681
JohnBrian <njugunajb96@gmail.com>
2023-12-07 05:30:23 +0300

    mount  controls to entrypoint

---
+++ CaterpillarControlSystem.control/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CaterpillarControlSystem.control
+{
+    internal abstract class Program
+    {
+        private static void Main()
+        {
+            var controlSystem = new Operations.CaterpillarControlSystem();
+            controlSystem.Run();
+        }
+    }
+}
