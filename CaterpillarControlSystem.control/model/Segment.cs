4cffd0fb42e6a5e7a04088326665cabe7dc577a4
JohnBrian <njugunajb96@gmail.com>
2023-12-07 05:29:17 +0300

    create Segment model

--- CaterpillarControlSystem.control/model/Segment.cs
+++ CaterpillarControlSystem.control/model/Segment.cs
@@ -2,5 +2,14 @@ namespace CaterpillarControlSystem.control.model;
 
 public class Segment
 {
-    
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Type { get; set; }
+
+    public Segment(int x, int y, char type)
+    {
+        X = x;
+        Y = y;
+        Type = type;
+    }
 }
